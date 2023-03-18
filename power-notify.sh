e4cac3691323edc6077cce3fad1e05cebb114247
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-03-18 21:48:01 +0530

    create power-notify.sh
    script to send notification when power exceeds range

---
+++ power-notify.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+while true
+do
+  	battery_level=`cat /sys/class/power_supply/BAT0/capacity`
+	
+	if [ $battery_level -ge 95 ]; then
+		notify-send "Battery Full" "Level: ${battery_level}%"
+	elif [ $battery_level -le 10 ]; then
+		notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
+	fi
+ 	
+	sleep 30
+done
