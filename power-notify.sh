30c54a893717e29ad1e7f2a2dcb11c1292a91e1c
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-04-20 21:13:48 +0530

    charge status check

--- power-notify.sh
+++ power-notify.sh
@@ -3,10 +3,11 @@
 while true
 do
   	battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+  	battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-	if [ "$battery_level" -ge 69 ]; then
+	if [[ "$battery_status" == "Charging" && "$battery_level" -ge 68 ]]; then
 		notify-send -t 3000 "Battery Full" "Level: ${battery_level}%"
-	elif [ "$battery_level" -le 10 ]; then
+	elif [[ "$battery_status" == "Discharging" && "$battery_level" -le 10 ]]; then
 		notify-send -t 3000 "Battery Low" "Level: ${battery_level}%"
 	fi
  	
