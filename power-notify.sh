c552c19fa0374a0fe40f9dc077c356b441ef444c
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-04-21 00:51:35 +0530

    accept arguments
    add usage display

--- power-notify.sh
+++ power-notify.sh
@@ -1,15 +1,46 @@
 #!/usr/bin/env bash
 
-while true
-do
-  	battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
-  	battery_status=$(cat /sys/class/power_supply/BAT0/status)
+usage() {
+	echo "usage: ./power-notify.sh <lower_limit> <upper_limit>"
+	echo "limits must be battery charge percentages."
+}
 
-	if [[ "$battery_status" == "Charging" && "$battery_level" -ge 68 ]]; then
-		notify-send -t 3000 "Battery Full" "Level: ${battery_level}%"
-	elif [[ "$battery_status" == "Discharging" && "$battery_level" -le 10 ]]; then
-		notify-send -t 3000 "Battery Low" "Level: ${battery_level}%"
+check_args() {
+	if [ "$#" -lt 2 ]; then
+		usage
+		exit 1
+	elif [ "$1" -eq "$2" ]; then
+		echo "Two limits must not be same!"
+		echo ""
+		usage
+		exit 1
 	fi
- 	
-	sleep 30
-done
+
+	if [ "$1" -gt "$2" ]; then
+		lower_limit="$2"
+		upper_limit="$1"
+	fi
+}
+
+start_listener() {
+	while true
+	do
+  		battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+  		battery_status=$(cat /sys/class/power_supply/BAT0/status)
+
+		if [[ "$battery_status" == "Charging" && "$battery_level" -ge "$upper_limit" ]]; then
+			notify-send -t 3000 "Battery Full" "Level: ${battery_level}%"
+		elif [[ "$battery_status" == "Discharging" && "$battery_level" -le "$lower_limit" ]]; then
+			notify-send -t 3000 "Battery Low" "Level: ${battery_level}%"
+		fi
+
+		sleep 60
+	done
+}
+
+
+lower_limit="$1"
+upper_limit="$2"
+
+check_args "$@"
+start_listener
