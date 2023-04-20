42b85c0d7df050bab2de366a9277822c5e3fe975
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-04-20 21:05:36 +0530

    add expiry time
    removed urgency level and add expiry time to notifications

--- power-notify.sh
+++ power-notify.sh
@@ -3,11 +3,11 @@
 while true
 do
   	battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
-	
-	if [ "$battery_level" -ge 90 ]; then
-		notify-send "Battery Full" "Level: ${battery_level}%"
+
+	if [ "$battery_level" -ge 69 ]; then
+		notify-send -t 3000 "Battery Full" "Level: ${battery_level}%"
 	elif [ "$battery_level" -le 10 ]; then
-		notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
+		notify-send -t 3000 "Battery Low" "Level: ${battery_level}%"
 	fi
  	
 	sleep 30
